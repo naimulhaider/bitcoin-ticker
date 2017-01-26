6721f6a25037205f6a3268d6052b0b26c4c35d55
Naimul Haider <naimulhaider@gmail.com>
2017-01-26 18:44:47 +0600

    Make terminal clear properly

--- printer.go
+++ printer.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
 	"time"
 )
+
+func ClearTerminal() {
+	if runtime.GOOS == "linux" {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else if runtime.GOOS == "windows" {
+		cmd := exec.Command("cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
 
 func Printer(data *Data, quit chan string) {
 	var btcUSD, btcEUR, eurUSD float64 = 0, 0, 0
 	var activeBTC, activeCUR int = 0, 0
 
 	printTicker := func() {
+		ClearTerminal()
 		tm := time.Now().Format(time.RFC3339)
 		fmt.Printf(
 			"\rLast Update: %v BTC/USD: %f   EUR/USD: %f   BTC/EUR: %f Active sources: BTC/USD (%d of %d)  EUR/USD (%d of %d)",
